75820c1219978db4065d8db79893a19433e2b5a6
Andreas Albert <andreas.albert@cern.ch>
2019-02-23 21:39:58 +0100

    Structure

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'nanopy',
-    version = '1.0.0',
+    version = '0.0.1',
     url = 'https://github.com/AndreasAlbert/nanopy.git',
     author = 'Andreas Albert',
     author_email = 'andreas.albert@cern.ch',
     description = 'HEP Analysis on flat TTrees, like CMS NanoAOD',
     packages = find_packages(),    
-    install_requires = ['numpy'],
+    install_requires = [''],
 )
